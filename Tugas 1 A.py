51fe4fea45234b5df8cd65d04d9101e0a80213f6
aszka-sazkia <aszkashifa3@gmail.com>
2024-09-23 10:50:26 +0700

    Add files via upload

---
+++ Tugas 1 A.py
@@ -0,0 +1,10 @@
+# Diketahui
+n = 1.50
+R1 = 22  # cm
+R2 = 17.5  # cm
+
+# Rumus untuk menghitung jarak fokus (f)
+f = 1 / ((n - 1) * (1 / R1 + 1 / R2))
+
+# Cetak hasil
+print("Jarak fokus lensa:", f, "cm")
